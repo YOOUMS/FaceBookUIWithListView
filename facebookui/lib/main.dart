a319c101cdb6e7e6345e37c251b6e5484e55d6e8
youms <yussef.msabeh@gmail.com>
2022-06-28 12:52:07 +0300

    facebook with listview

---
+++ facebookui/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:facebookui/facebookUI.dart';
+import 'package:facebookui/facebookUi2.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: facebookUI()));
+  // runApp(MaterialApp(home: facebookUI2()));
+}
